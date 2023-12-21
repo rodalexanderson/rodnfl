5058e0feb6526e7e6aaa028883cc004288f21c21
rodalexanderson <73920054+rodalexanderson@users.noreply.github.com>
2023-12-20 21:07:50 -0600

    changes in home page, deleting bg image

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import moment from 'moment'
 import Link from 'next/link'
 
-const PostCard = ({post}) => {
-
+const PostCard = ({post, key}) => {
+  console.log(post)
   return (
-    <div className='bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8'>
+    <div className='bg-gray-50 shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8'>
         <div className='relative overflow-hidden shadow-md pb-80 mb-6'>
           <img src={post.featuredImage.url} alt={post.title} className="object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg"/>
         </div>
